5b50ebd684be7c666b5a4ea2e80156742e53078c
SamanthaHeck <sjh897@gmail.com>
2018-10-12 11:18:38 -0700

    Barcode bleed/contamination
    Some cleanup plus edits to barcode bleed/contamination checking.

--- PostReport.py
+++ PostReport.py
@@ -2,13 +2,54 @@ from modules.IdentifiedBarcodes import IdentifiedBarcodes
 from modules.SampleSheet import SampleSheet
 from modules.PrimerSheet import PrimerSheet
 from modules.BarcodeLookup import BarcodeLookup
+import modules.Validation as Validation
 from datetime import datetime
 import sys
+
+def HammingDist(p1, p2, s1, s2):
+	ss = s1 if len(s1) < len(s2) else s2
+	sl = s1 if len(s1) >= len(s2) else s2
+	ps = p1 if len(s1) < len(s2) else p2
+	pl = p1 if len(s1) >= len(s2) else p2
+	lenDiff = len(sl) - len(ss)
+	minDist = float('inf');
+	for i in range(4):
+		diffs = i;
+		if (lenDiff < i):
+			diffs += i - lenDiff
+		matches = []
+		j = 0
+		for k in range(i,len(sl)):
+			if j >= len(ss):
+				break
+			if (Validation.CheckNucleotidesDifferent(ss[j],sl[k])):
+				diffs += 1
+			else:
+				matches.append((j,k))
+			j += 1
+		minDist = min(diffs,minDist)
+		diffs = i
+		if (lenDiff < i):
+			diffs += i - lenDiff
+		matches = []
+		j = 0
+		for k in range(i,len(sl)):
+			if k >= len(ss):
+				break
+			if (Validation.CheckNucleotidesDifferent(ss[k],sl[j])):
+				diffs += 1
+			else:
+				matches.append((k,j))
+			j += 1
+		minDist = min(diffs,minDist)
+	return minDist
+
 
 sampleSheetName = None
 primerSheetName = None
 barcodeLookupName = None
 identifiedBarcodesName = None
+supressBarcodeWarning = False
 
 if (len(sys.argv) > 0):
 	count = 0
@@ -18,6 +59,7 @@ if (len(sys.argv) > 0):
 			print("-P FILENAME, --primer_file FILENAME\tfile with primers")
 			print("-S FILENAME, --sample_metadata FILENAME\tfile with sample metadata")
 			print("-I FILENAME, --identified_barcodes FILENAME\tfile with identified barcodes")
+			print("-E\tSuppress warnings for barcodes not in the sample sheet.")
 			exit(0)
 		if sys.argv[count] == "--sample_metadata" or sys.argv[count] == '-S':
 			count += 1
@@ -35,6 +77,9 @@ if (len(sys.argv) > 0):
 			count += 1
 			if (count < len(sys.argv)):
 				identifiedBarcodesName = sys.argv[count]
+		elif (sys.argv[count] == "-E"):
+			supressBarcodeWarning = True
+			count += 1
 		count += 1
 
 if sampleSheetName == None:
@@ -101,10 +146,53 @@ for i in range(1,len(spreadSheet)-1):
 	if (readSum > 10):
 		percent = int(spreadSheet[i][-1]) / readSum
 		if (len(primers) == 0):
-			print("Warning: Barcode %s has %d reads, %d%% of which match no primers, but does not appear in the sample sheet." %(barcode, readSum, percent * 100))
+			if (supressBarcodeWarning == False):
+				print("Warning: Barcode %s has %d reads, %d%% of which match no primers, but does not appear in the sample sheet. " %(barcode, readSum, percent * 100), end='')
+				closest = None
+				l1 = None
+				l2 = None
+				dist = float('inf')
+				for i in range(len(barcodeLookup.barcodes)):
+					if i == barcodeIndex or barcodeLookup.barcodeNames[i] not in sampleSheet.barcodeIDs:
+						continue;
+					check1 = HammingDist(barcode,barcodeLookup.barcodeNames[i],barcodeLookup.barcodes[barcodeIndex][0],barcodeLookup.barcodes[i][0])
+					check2 = HammingDist(barcode,barcodeLookup.barcodeNames[i],barcodeLookup.barcodes[barcodeIndex][1],barcodeLookup.barcodes[i][1])
+					#print("%d %d" %(check1,check2))
+					thisDist = check1 + check2
+					if thisDist < dist:
+						dist = thisDist
+						closest = barcodeLookup.barcodeNames[i]
+						l1 = barcodeLookup.barcodes[i][0]
+						l2 = barcodeLookup.barcodes[i][1]
+				if (dist < 3):
+					print("Possible barcode bleed: l1:%s, l2:%s. Possible source: %s l1:%s, l2:%s" %(barcodeLookup.barcodes[barcodeIndex][0],barcodeLookup.barcodes[barcodeIndex][1],closest,l1,l2))
+				else:
+					print("Possible barcode contamination: l1:%s, l2:%s. Possible source: %s l1:%s, l2:%s" %(barcodeLookup.barcodes[barcodeIndex][0],barcodeLookup.barcodes[barcodeIndex][1],closest,l1,l2))
 		else:
 			if percent >= 0.1:
-				print("Warning: %d%% of %d read(s) for %s did not match a primer." %(percent * 100,readSum,barcode))
+				print("Warning: %d%% of %d read(s) for %s did not match a primer. " %(percent * 100,readSum,barcode), end='')
+				closest = None
+				l1 = None
+				l2 = None
+				dist = float('inf')
+				for i in range(len(barcodeLookup.barcodes)):
+					if i == barcodeIndex or barcodeLookup.barcodeNames[i] not in sampleSheet.barcodeIDs:
+						continue;
+					check1 = HammingDist(barcode,barcodeLookup.barcodeNames[i],barcodeLookup.barcodes[barcodeIndex][0],barcodeLookup.barcodes[i][0])
+					check2 = HammingDist(barcode,barcodeLookup.barcodeNames[i],barcodeLookup.barcodes[barcodeIndex][1],barcodeLookup.barcodes[i][1])
+					#print("%d %d" %(check1,check2))
+					thisDist = check1 + check2
+					if thisDist < dist:
+						dist = thisDist
+						closest = barcodeLookup.barcodeNames[i]
+						l1 = barcodeLookup.barcodes[i][0]
+						l2 = barcodeLookup.barcodes[i][1]
+				if (dist < 3):
+					print("Possible barcode bleed: l1:%s, l2:%s. Possible source: %s l1:%s, l2:%s" %(barcodeLookup.barcodes[barcodeIndex][0],barcodeLookup.barcodes[barcodeIndex][1],closest,l1,l2))
+				else:
+					print("Possible barcode contamination: l1:%s, l2:%s. Possible source: %s l1:%s, l2:%s" %(barcodeLookup.barcodes[barcodeIndex][0],barcodeLookup.barcodes[barcodeIndex][1],closest,l1,l2))
+	#else:
+	#	print("Barcode %s is fine." %barcode)
 	
 for project in sampleSheet.projectIDs:
 	file = open("%s_%s.txt" %(datetime.now().strftime("%Y%m%d"), project),"w")
